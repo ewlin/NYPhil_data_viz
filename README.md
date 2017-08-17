b96439d9872d11da3f042714d4fe9505dae9e172
ewlin <ericwilliamlin@gmail.com>
2017-08-17 17:13:43 -0400

    wiki API grabbing birth and death years of composers; almost there

--- README.md
+++ README.md
@@ -3,11 +3,18 @@
 ### _(Updated 7/19/2017)_
 
 ### TODOs: 
-#### High Priority: 
+
+* Finish Wiki scraping so grab as many birth and death dates as possible and add to composers array
+* Manually find missing dates 
+
+
 * Add Tooltips
-* Categories and Color Schemes
+* Categories and Color Schemes (Categories for Genres)
 * Finish categorizing all works by the TOP 50 Most Performed Composers 
 
 #### Medium-term features: 
 
+
 #### Possible additional routes: 
+
+* Composer and Conductor nationalities
--- wiki_data.js
+++ wiki_data.js
@@ -1,31 +1,25 @@
-//d3.json('compositions.json', (d) => {
-//	d.forEach(composer => {
-//		//console.log(composer.composer);
-//		let composer_name = composer.composer.split(",").reverse().join(); 
-//		$.ajax({
-//  		url: '//en.wikipedia.org/w/api.php',
-//  		data: { action: 'query', search: composer_name, format: 'json' },
-//  		dataType: 'jsonp',
-//  		success: function (x) {
-//  		  console.log(x);
-//  		}
-//		});
-//	}); 
-//}); 
+let composers = []; 
 
-//$.ajax({
-//  url: "https://en.wikipedia.org/wiki/Giovanni_da_Palestrina",
-//  dataType: 'jsonp',
-//  success: function (x) {
-//    console.log(x);
-//  }
-//});
+d3.json('compositions.json', (d) => {
+	d.forEach(composer => {
 
-$.ajax({
-  url: '//en.wikipedia.org/w/api.php',
-  data: { action: 'query', titles: "Timo Andres", format: 'json', prop: "extracts" },
-  dataType: 'jsonp',
-  success: function (x) {
-    console.log(x);
-  }
-});
+		let composer_name = composer.composer.split(",").reverse().join(" "); 
+		
+		//TODO Need to do query and grab titles first using search API, THEN, do a query based on Title
+		
+		
+		$.ajax({
+  		url: '//en.wikipedia.org/w/api.php',
+  		data: { action: 'query', titles: composer_name, format: 'json', prop: "categories" },
+  		dataType: 'jsonp',
+  		success: function (data) {
+  		  let id = Object.keys(data.query.pages)[0]; 
+				console.log(data.query.pages[id]);
+  		}
+		});
+		
+		
+		
+	}); 
+}); 
+
