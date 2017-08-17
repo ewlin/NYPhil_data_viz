58065fbbfd1ebbae9ce86b1b2e1e30cbe08ff55d
ewlin <ericwilliamlin@gmail.com>
2017-08-17 16:03:34 -0400

    wiki API experiments

--- wiki_data.js
+++ wiki_data.js
@@ -1,6 +1,29 @@
+//d3.json('compositions.json', (d) => {
+//	d.forEach(composer => {
+//		//console.log(composer.composer);
+//		let composer_name = composer.composer.split(",").reverse().join(); 
+//		$.ajax({
+//  		url: '//en.wikipedia.org/w/api.php',
+//  		data: { action: 'query', search: composer_name, format: 'json' },
+//  		dataType: 'jsonp',
+//  		success: function (x) {
+//  		  console.log(x);
+//  		}
+//		});
+//	}); 
+//}); 
+
+//$.ajax({
+//  url: "https://en.wikipedia.org/wiki/Giovanni_da_Palestrina",
+//  dataType: 'jsonp',
+//  success: function (x) {
+//    console.log(x);
+//  }
+//});
+
 $.ajax({
   url: '//en.wikipedia.org/w/api.php',
-  data: { action: 'opensearch', search: 'Richard Feynman', format: 'json' },
+  data: { action: 'query', titles: "Timo Andres", format: 'json', prop: "extracts" },
   dataType: 'jsonp',
   success: function (x) {
     console.log(x);
