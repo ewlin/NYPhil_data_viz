d09e7f17dde39b0d75595331e50faa2979cd14e8
ewlin <ericwilliamlin@gmail.com>
2017-10-25 19:09:37 -0400

    test

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,4 +1,10 @@
 function g(hello) {
   if (hello == 5) return 'test success'; 
   return 'not sucess';
+} 
+function test(tree) {
+  if (test) {
+    return test === 5 ? test : 7; 
+  } 
+  return 9;
 }
