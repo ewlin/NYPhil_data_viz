6aa1589b8f878410ac9c3662f2d7d00349c7fed0
ewlin <ericwilliamlin@gmail.com>
2017-11-06 23:02:23 -0500

    mobile test

--- assets/js/stream_experiment.js
+++ assets/js/stream_experiment.js
@@ -90,10 +90,10 @@ $('.explain p').css('margin-bottom', function() {
 
                     
 //GITHUB pages bug 
-d3.json('/NYPhil_data_viz/data/composers.json', (err, d) => {
+//d3.json('/NYPhil_data_viz/data/composers.json', (err, d) => {
 
 //DEV
-//d3.json('../../data/composers.json', (err, d) => {
+d3.json('../../data/composers.json', (err, d) => {
 	
   d.forEach( (composer, composerIdx) => {  //[] of work objects
     let works = composer.works,
@@ -802,7 +802,12 @@ d3.json('/NYPhil_data_viz/data/composers.json', (err, d) => {
 
   }
 
-  window.addEventListener('resize', debounce(resize, 200)); 
+  window.addEventListener('resize', function () {
+    if (!isMobile().any()) {
+      console.log('not mobile');
+      debounce(resize, 200); 
+    }
+  }); 
 	
 }); 
 
