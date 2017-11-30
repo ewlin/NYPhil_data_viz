9ddb535146fa82dbb2eff45703acbadb50dfb68d
ewlin <ericwilliamlin@gmail.com>
2017-11-30 09:02:21 -0500

    scrolly bug; remove at-bottom class when scrolling back in reverse

--- bundled-babel/streamBundle.js
+++ bundled-babel/streamBundle.js
@@ -855,6 +855,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 		scene.on('enter', function () {
 			//console.log('fixed'); 
 			$('.inner-container').addClass('fixed');
+			$('.inner-container').removeClass('at-bottom');
 		});
 
 		scene.on('leave', function (e) {
